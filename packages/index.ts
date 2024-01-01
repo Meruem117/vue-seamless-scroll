c2c18f8f5d837e17d832be936ef1e73cd2a0af09
Meruem117 <1572677319@qq.com>
2024-01-02 00:01:23 +0800

    commit

--- packages/index.ts
+++ packages/index.ts
@@ -1,8 +1,8 @@
 import type { App } from 'vue'
 import VueSimpleScroll from './components/vue-simple-scroll.vue'
 
-VueSimpleScroll.install = (app: App) => {
-    app.component(VueSimpleScroll.name, VueSimpleScroll)
-}
-
-export default VueSimpleScroll
+export default ({
+    install: (app: App) => {
+        app.component(VueSimpleScroll.name, VueSimpleScroll)
+    }
+})
