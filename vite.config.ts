7b5b34603800d6e751739218b3ca1bbc9e2b9dc5
Meruem117 <1572677319@qq.com>
2024-01-01 21:53:00 +0800

    tmp

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts()
+  ],
+  build: {
+    lib: {
+      entry: 'packages/index.ts',
+      name: 'vue-simple-scroll',
+      fileName: (format) => `vue-simple-scroll.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   server: {
     port: 3000
-  }
+  },
 })
